1fec5f948ad27f31f51ace7f576f3eaaff3150e1
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-07-29 10:27:10 +0000

    done

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1>Mega Project</h1>
+    </>
+  )
+}
+
+export default App
