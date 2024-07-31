13a10dafd4976165e4854501fa0e41aba2c30b30
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-07-31 13:01:40 +0000

    done

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,14 +1,40 @@
-
+import {useDispatch} from 'react-redux'
+import React,{ useState,useEffect } from 'react'
 import './App.css'
-
+import authservice from './appwrite/auth'
+import {login,logout} from "./store/authSlice"
+import { Header } from './components'
+import {Footer} from './components'
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading,setloading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Mega Project</h1>
-    </>
-  )
+  useEffect(()=>{
+    authservice.getCurrentUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      }
+      else
+      {
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setloading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+
+        TODO:{/* {<Outlet/>} */}
+      </main>
+      <Footer/>
+    </div>
+    </div>
+  ) : null
 }
 
 export default App
--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from "../config/config";
+import conf from "../config/config.js";
 import {Client,Account,ID} from "appwrite";
 
 export class AuthService{
