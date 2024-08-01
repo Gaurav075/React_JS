d9024d2febe2fde6aff543cba6ca2381abb9dcb5
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-08-01 13:10:52 +0000

    done

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,7 +1,12 @@
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
+import Container from './container/Container'
+import Logo from './Logo';
+import Logout from './Header/Logout';
 
 export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo
 }
